20a2cd2f7a39d5827ee9912299d4d7e6b5a7e96d
command7 <vijay_saravanan@outlook.com>
2018-10-10 01:19:04 -0400

    Make Vehicle class abstract since interface requires abstract gasMileage() function

--- Vehicle.java
+++ Vehicle.java
@@ -10,7 +10,7 @@
 *@version   1.0
 */
 import java.util.*;
-public class Vehicle implements Vinfo
+public abstract class Vehicle implements Vinfo
 {
    /** Stores the model of the vehicle */
    private String model;
