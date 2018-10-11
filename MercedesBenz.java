b9d1e4d641025980bd49f12f6dba9c3016a84ce2
command7 <vijay_saravanan@outlook.com>
2018-10-11 16:17:20 -0400

    Add outline of MercedesBenz Class

---
+++ MercedesBenz.java
@@ -0,0 +1,35 @@
+public class MercedesBenz extends Vehicle
+{
+   private int typeOfMake = -1;
+   private int typeOfDrive = -1;
+   private final String [] allMakes = {"Coupe", "Sedan", "SUV", "Cabriolet"};
+   private final String [] allDrives = {"FWD", "4Matic"};
+   
+   public MercedesBenz ()
+   {
+   }
+   
+   public String getTypeOfMake()
+   {
+   }
+   
+   public String getTypeOfDrive()
+   {
+   }
+   
+   public void setTypeOfMake(int _typeOfMake)
+   {
+   }
+   
+   public void setTypeOfDrive(int _typeOfDrive)
+   {
+   }
+   
+   public double gasMileage()
+   {
+   }
+   
+   public String toString()
+   {
+   }
+}
