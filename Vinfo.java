fede52814ead96ca7f9af35f310c39f373819001
command7 <vijay_saravanan@outlook.com>
2018-10-11 21:47:14 -0400

    Shift MPG constants to Vinfo Interface from respective classes

--- Vinfo.java
+++ Vinfo.java
@@ -1,4 +1,16 @@
 public interface Vinfo {
+   final static double sedanMpg = 23.7;
+   /** Mileage of a Coupe car*/
+   final static double coupeMpg = 28.2;
+   /** Mileage of a Wagon car*/
+   final static double wagonMpg = 19.5;
+   /** Mileage that is to be deducted if the car has towing capability */
+   final static double towingMpg = 3;
+   /** Mileage of a Pontoon boat */
+   final static double pontoonMpg = 3.5;
+   /** Mileage of a PWC boat*/
+   final static double pwcMpg = 2.2;
+   /** Mileage of a SailBoat */
+   final static double sailBoatMpg = 0;
 	public abstract double gasMileage();
-  
 }
