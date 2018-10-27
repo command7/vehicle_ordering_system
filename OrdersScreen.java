5cec357548de4ee69363ac5f28c389b097bab586
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2018-10-27 13:23:24 -0400

    Create JPanel for vehicle options and add to Frame with Border Layout

--- OrdersScreen.java
+++ OrdersScreen.java
@@ -5,9 +5,13 @@ import java.awt.event.*;
 public class OrdersScreen extends JFrame
 {
    private String [] vehicleTypes = {"Car", "Boat", "Truck", "Sled", "Mercedes Benz"};
+   
    private JLabel vehicleTypeLabel;
    private JLabel windowHeader;
    private JComboBox vehicleTypeComboBox;
+   private JLabel modelLabel;
+   private JComboBox modelComboBox;
+   private JPanel comboBoxes = new JPanel();
    public OrdersScreen()
    {
       createFrame();
@@ -15,21 +19,33 @@ public class OrdersScreen extends JFrame
    
    public void createFrame()
    {
+      this.setLayout(new BorderLayout());
             
       String headerText = "<html><b><font color='red' size=5>Student Management System</font><b></html>";
-  		windowHeader = new JLabel(headerText);
-      this.add(windowHeader);
+  		windowHeader = new JLabel(headerText, SwingConstants.CENTER);
+      this.add(windowHeader, BorderLayout.NORTH);
 
       vehicleTypeLabel = new JLabel("Vehicle Type", SwingConstants.RIGHT);
       vehicleTypeComboBox = new JComboBox(vehicleTypes);
       this.add(vehicleTypeLabel);
       this.add(vehicleTypeComboBox);
       
+      modelLabel = new JLabel("Vehicle Type", SwingConstants.RIGHT);
+      modelComboBox = new JComboBox(vehicleTypes);
+      this.add(modelLabel);
+      this.add(modelComboBox);
+      
+      comboBoxes.setLayout(new FlowLayout(FlowLayout.CENTER));
+      comboBoxes.add(vehicleTypeComboBox);
+      comboBoxes.add(modelComboBox);
+      this.add(comboBoxes,BorderLayout.EAST);
+      
       this.setTitle("Orders Screen");
       this.setSize(600,300);
       this.setVisible(true);
       this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 
+      
       
       
    }
